2a88223c589d34ef6010ccb522b8e9402db547ec
rufus design <rufusonfire0307@gmail.com>
2025-11-18 14:46:14 +0900

    Update script-about.js

--- docs/script-about.js
+++ docs/script-about.js
@@ -16,3 +16,25 @@ reveals2.forEach(r => io.observe(r));
 reveals2.forEach(r => r.classList.add('is-visible'));
 }
 
+
+
+
+// CLICK TO REVEAL ABOUT //
+const overlay = document.getElementById("about-full-screen-overlay-wrapper");
+const container = document.getElementById("about-description-container");
+const trigger = document.getElementById("about-seiko-cartoon");
+
+// Open overlay when clicking the cartoon div
+trigger.addEventListener("click", function (event) {
+  overlay.style.display = "flex";
+  event.stopPropagation(); // prevent click from instantly closing it
+});
+
+// Close overlay only when clicking outside the container
+overlay.addEventListener("click", function (event) {
+  const clickedInside = container.contains(event.target);
+  if (!clickedInside) {
+    overlay.style.display = "none";
+  }
+});
+
