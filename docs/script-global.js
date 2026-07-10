72a8736881344d766abcdd6f3936338617a0e336
rufus design <rufusonfire0307@gmail.com>
2026-07-10 16:30:51 +0200

    Update script-global.js

--- docs/script-global.js
+++ docs/script-global.js
@@ -37,7 +37,7 @@ class PageTransition {
     window.addEventListener("load", () => {
       requestAnimationFrame(() => this.overlay.classList.add("enter"));
     });
-    window.addEventListener('popstate', (event) =>  {
+    window.addEventListener('beforeunload', (event) =>  {
     this.overlay.classList.remove("exit");
     this.overlay.classList.add("enter");
   });
