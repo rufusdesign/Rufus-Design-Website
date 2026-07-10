8ab3dfcfb8cb4565843b2d98f2687a6726e85b3f
rufus design <rufusonfire0307@gmail.com>
2026-07-10 20:01:59 +0200

    Update script-global.js

--- docs/script-global.js
+++ docs/script-global.js
@@ -19,12 +19,6 @@
 // Run once on first load
   highlightNav();
 
-// Animate overlay down on back button
-   window.onpopstate = (event) =>  {
-   this.overlay.classList.remove("exit");
-   this.overlay.classList.add("enter");
-  };
-
 // PAGE TRANSITION //
 // page-transition.js
 class PageTransition {
@@ -40,7 +34,11 @@ class PageTransition {
     window.addEventListener("load", () => {
       requestAnimationFrame(() => this.overlay.classList.add("enter"));
     });
-  
+    // Animate overlay down on back button
+    window.onpopstate = (event) =>  {
+    this.overlay.classList.remove("exit");
+    this.overlay.classList.add("enter");
+  };
     this.bindInternalLinks(document);
   }
 
